04e12462cf9b5781b9486b6ac9c6c1700800e955
tim2803 <57104390+tim2803@users.noreply.github.com>
2020-02-27 18:30:19 +0200

    завдання станом на 27,02,20

--- demo01/routes/index.js
+++ demo01/routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res) {
     res.send('Hello , ' + req.query.name);
 });
 
+router.get('/make-short-name', function(req, res) {
+    // res.render('index', { title: 'Express' });
+    res.send('Hello ,' + req.query.firstname.substr(0, 1) + '.' + req.query.secondname);
+});
 module.exports = router;
