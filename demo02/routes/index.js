c86897130a196f27c0a6b963183db4bed244af3f
tim2803 <57104390+tim2803@users.noreply.github.com>
2020-03-05 18:33:16 +0200

    практически доделаная домашка  05,03,20

--- demo02/routes/index.js
+++ demo02/routes/index.js
@@ -3,18 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', {
-        title: 'My Demo Prog'
-    });
+    res.render('index', { title: 'My Demo' });
 });
 
 
+
 router.post('/', function(req, res) {
+    if (isNaN(res.body.firstname) === false && isNaN(res.body.lastname) === false) {
+        var a = parseInt(res.body.firstname) + parseInt(res.body.lastname);
+        var b = parseInt(res.body.firstname) - parseInt(res.body.lastname);
+        var c = parseInt(res.body.firstname) * parseInt(res.body.lastname);
+    }
     res.render('index', {
         title: 'Method POST result:',
-        // FirstName: req.body.firstname.substar(0, 1),
-        // LastName: req.body.lastname
-        Greeting: 'Hello ' + req.body.firstname.substr(0, 1) + '.' + req.body.lastname
+        Greeting: 'hello' + a + ' ' + b + ' ' + c
     });
 });
 module.exports = router;
